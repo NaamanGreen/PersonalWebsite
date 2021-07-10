58a2459fbe3e629d613a743cb9bbf521c35a0783
Naaman Green <naamangreen@brandeis.edu>
2021-07-10 14:45:02 -0400

    Contact form now working

--- contact/contactProcessor.php
+++ contact/contactProcessor.php
@@ -1,42 +1,13 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Contact Processor Page</title>
-</head>
-
-<body>
-
-<h1>Processed Data</h1>
-
 <?php
-	if(isset($_POST['send']))
-	{
-		
-		$name = $_POST['name'];
-		$company = $_POST['company'];
-		$email = $_POST['email'];
-		$message = $_POST['message'];
-		
-		if(!empty($name) && !empty($email) && !empty($email) && !empty($message))
-		{
-			$re_email = "/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/";
-
-			if(preg_match($re_email, $email))
-			{
-				echo "<p>name: $name</p>";
-				echo "<p>company: $company</p>";
-				echo "<p>email: $email</p>";
-				echo "<p>Message: $message</p>";
-				
-			}
-			else { echo "You did not format your email properly"; }
-		}
-		
-		else { echo "You did not fill in the required fields"; }
-	
-	}
+    $name = $_POST['name'];
+    $company = $_POST['company'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    $formcontent=" From: $name \n Company: $company \n Message: $message";
+    $recipient = "ng@naamangreen.com";
+    $subject = "Contact Form - NaamanGreen.com";
+    $mailheader = "From: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    echo "Thank You!" . " -" . "<a href='../' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
-
-</body>
-</html>
